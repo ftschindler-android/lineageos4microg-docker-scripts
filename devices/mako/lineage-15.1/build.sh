eee4bf406fd173c126b8ecafd6b6805ed7d0de9d
Felix Schindler <felix@localhost>
2019-09-26 07:26:02 +0000

    [mako.lineage-15.1] add first draft

---
+++ devices/mako/lineage-15.1/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+THISDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P )"
+BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})/../../.." ;  pwd -P )"
+
+for ii in zips keys; do
+    mkdir -p ${BASEDIR}/$ii
+done
+for ii in src logs manifests ccache; do
+    mkdir -p ${THISDIR}/$ii
+done
+
+# disable ccache due to https://github.com/ccache/ccache/issues/153
+
+docker run \
+    --memory=10G \
+    --kernel-memory=10G \
+    --cpus=4 \
+    -e "BRANCH_NAME=lineage-15.1" \
+    -e "DEVICE_LIST=mako" \
+    -e "INCLUDE_PROPRIETARY=true" \
+    -e "SIGN_BUILDS=true" \
+    -e "SIGNATURE_SPOOFING=restricted" \
+    -e "CUSTOM_PACKAGES=GmsCore GsfProxy FakeStore MozillaNlpBackend NominatimNlpBackend FDroid FDroidPrivilegedExtension OpenWeatherMapWeatherProvider" \
+    -e "USE_CCACHE=0" \
+    -e "CCACHE_SIZE=1M" \
+    -e "LOGS_SUBDIR=false" \
+    -v "${BASEDIR}/zips:/srv/zips" \
+    -v "${BASEDIR}/keys:/srv/keys" \
+    -v "${THISDIR}/src:/srv/src" \
+    -v "${THISDIR}/logs:/srv/logs" \
+    -v "${THISDIR}/manifests:/srv/local_manifests" \
+    -v "${THISDIR}/ccache:/srv/ccache" \
+    lineageos4microg/docker-lineage-cicd
+
